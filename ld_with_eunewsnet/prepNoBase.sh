2cb03b1f72de4f2a9dccd5d8e1ac1e9ee727a9a4
Daniel Draper <daniel.draper@student.kit.edu>
2017-03-31 12:06:43 +0200

    Added most recent logs

--- ld_with_eunewsnet/prepNoBase.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-FILE=$1
-grep "Length of sample" -B 2 $FILE > length/testLength
-sed -i "s/Should have been: //g" length/testLength
-sed -i "s/Overall we have classified as: //g" length/testLength
-sed -i "s/Length of sample: //g" length/testLength
-sed -i "s/--//g" length/testLength
-sed -i "s/Should Eq: //g" length/testLength
